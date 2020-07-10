ba149f23b74b57ad4ebec2bc045598ecd0f053f8
Abhishek Bedi <kbedi@mafcarrefour.com>
2020-07-10 14:05:16 +0400

    Initial Commit

---
+++ WidgetDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetDemo
+//
+//  Created by kbedi on 10/07/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
